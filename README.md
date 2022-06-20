7c29874fbfed47bd304ea91b04db20fdfa006a58
drd4games <tsenisda@gmail.com>
2022-06-20 12:51:20 +0300

    3. router dom and switching to components

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # React Eshop
 
 # fully functional eshop using react
+
+## what you will find on commits
+
+### 1. just creating the base
+
+### 2. adding some producs
+
+### 3. change the main div to 2 components
+
+    React-router-dom for changing pages inside the app
+    using params to get the page we are on
+    change anchor tags with Link (to) to stop refreshing when change pages
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/"> Eat-Now </a>
-      </header>
-      <main>
-        <h1> Featured Salads</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/"> Eat-Now </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
